68a82c47b4c21f8891f07fa77f05e33b4a8f333e
Muriithi <muriithi.kibata@student.moringaschool.com>
2022-08-16 22:11:09 +0300

    Completed Assignment

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
 // code your solution here
+function saturdayFun(activity = "roller-skate"){
+    return `This Saturday, I want to ${activity}!`
+}
+saturdayFun("bath my dog")
+saturdayFun()
+function mondayWork (plan = "go to the office") {
+    return `This Monday, I will ${plan}.`
+}
+mondayWork("work from home.")
+function wrapAdjective(symbol = "*") {
+return function(saying = "special") {
+    return `You are ${symbol}${saying}${symbol}!`
+}
+    
+    
+}
+ 
